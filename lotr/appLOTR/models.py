330b7b4cac6ba0e57b7608f7dd9b35f160059491
markelretes <93277461+markelretes@users.noreply.github.com>
2023-10-31 13:35:44 +0100

    Creación de modelos

--- lotr/appLOTR/models.py
+++ lotr/appLOTR/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+ 
+class Pelicula(models.Model):
+    # No es necesario crear un campo para la Primary Key, Django creará automáticamente un IntegerField.
+    titulo = models.CharField(max_length=100)
+    anyo = models.IntegerField()
+    director = models.CharField(max_length=50)
+    duracion = models.IntegerField()
+    genero = models.CharField(max_length=30)
 
-# Create your models here.
+class Raza(models.Model):
+    # No es necesario crear un campo para la Primary Key, Django creará automáticamente un IntegerField.
+    nombre = models.CharField(max_length=20)
+ 
+class Personaje(models.Model):
+    # Campo para la relación one-to-many (un empleado pertenece a un departamento)
+    raza = models.ForeignKey(Raza, on_delete=models.CASCADE)
+    # Campo para la relación many-to-many (un empleado tiene varias habilidades)
+    pelicula = models.ManyToManyField(Pelicula)
+    nombre = models.CharField(max_length=50)
+    genero = models.CharField(max_length=20)
+    colorOjos = models.CharField(max_length=15)
+    colorPelo = models.CharField(max_length=15)
+    estatura = models.IntegerField()
+    # Es posible indicar un valor por defecto mediante 'default'
+    imagen = models.ImageField(upload_to ='img/')
+    # Para permitir propiedades con valor null, añadiremos las opciones null=True, blank=True. 
