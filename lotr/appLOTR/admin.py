c2c2a3dddf32ea9e3608a356161dcf833b9adde5
markelretes <93277461+markelretes@users.noreply.github.com>
2023-11-03 18:48:17 +0100

    Añadir personajes

--- lotr/appLOTR/admin.py
+++ lotr/appLOTR/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Pelicula, Raza, Personaje
+admin.site.register(Pelicula)
+admin.site.register(Raza)
+admin.site.register(Personaje)
