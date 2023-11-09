4070d787ddc5591f983cfaf2a76b92a428ec80a1
Juan Orts <91051695+juanorts@users.noreply.github.com>
2023-11-09 18:55:38 +0100

    Añadir urls de personajes

--- lotr/appLOTR/urls.py
+++ lotr/appLOTR/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('razas/', views.index_razas, name='razas'),
     path('razas/<int:raza_id>', views.show_raza, name='raza'),
+    path('personajes/', views.index_personajes, name='personajes'),
+    path('personajes/<int:personaje_id>', views.show_personaje, name='personaje'),
 ]
