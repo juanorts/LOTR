139024c889744938ec5deca0e0b8b3c412dd4576
Juan Orts <91051695+juanorts@users.noreply.github.com>
2023-11-09 18:45:45 +0100

    Añadir views y templates de personajes
    TODO: añadir lista de películas a cada personaje

--- lotr/appLOTR/models.py
+++ lotr/appLOTR/models.py
@@ -10,6 +10,9 @@ class Pelicula(models.Model):
     #Portada de la pelicula
     imagen = models.ImageField(upload_to = 'img/', default = 'img/default.jpg')
 
+    def __str__(self):
+        return self.titulo
+
 class Raza(models.Model):
     # No es necesario crear un campo para la Primary Key, Django creará automáticamente un IntegerField.
     nombre = models.CharField(max_length=20, default = 'null')
@@ -19,11 +22,14 @@ class Raza(models.Model):
     longevidad = models.CharField(max_length=20, default = 'null')
     #Personaje promedio de la raza
     imagen = models.ImageField(upload_to='img/', default = 'img/default.jpg')
+
+    def __str__(self):
+        return self.nombre
  
 class Personaje(models.Model):
-    # Campo para la relación one-to-many (un empleado pertenece a un departamento)
+    # Campo para la relación one-to-many (un empleado pertenece a una raza)
     raza = models.ForeignKey(Raza, on_delete=models.CASCADE)
-    # Campo para la relación many-to-many (un empleado tiene varias habilidades)
+    # Campo para la relación many-to-many (un personaje aparece en varias peliculas)
     pelicula = models.ManyToManyField(Pelicula)
     nombre = models.CharField(max_length=50)
     genero = models.CharField(max_length=20)
@@ -33,3 +39,6 @@ class Personaje(models.Model):
     # Es posible indicar un valor por defecto mediante 'default'
     imagen = models.ImageField(upload_to ='img/', default = 'img/default.jpg')
     # Para permitir propiedades con valor null, añadiremos las opciones null=True, blank=True. 
+
+    def __str__(self):
+        return self.nombre
--- lotr/appLOTR/views.py
+++ lotr/appLOTR/views.py
@@ -15,3 +15,14 @@ def show_raza(request, raza_id):
     personajes = get_list_or_404(Personaje.objects.filter(raza_id=raza_id))
     context = {'raza': raza, 'personajes': personajes}
     return render(request, 'raza.html', context)
+
+def index_personajes(request):
+	personajes = get_list_or_404(Personaje.objects.order_by('nombre'))
+	context = {'personajes' : personajes}
+	return render(request, 'personajes.html', context)
+
+#devuelve los detalles de un personaje
+def show_personaje(request, personaje_id):
+	personaje = get_object_or_404(Personaje, pk=personaje_id)
+	context = { 'personaje': personaje}
+	return render(request, 'personaje.html', context)
