31bebae4e063e7001e60d98d3b35d1175a05ad3a
AndoniOka <91051757+AndoniOka@users.noreply.github.com>
2023-11-09 21:07:09 +0100

    Añadir views y templates de peliculas y index
    TODO: Falta CSS

--- lotr/appLOTR/views.py
+++ lotr/appLOTR/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, get_list_or_404
 from django.shortcuts import render
@@ -26,3 +26,24 @@ def show_personaje(request, personaje_id):
 	personaje = get_object_or_404(Personaje, pk=personaje_id)
 	context = { 'personaje': personaje}
 	return render(request, 'personaje.html', context)
+
+def index_peliculas(request):
+	peliculas = get_list_or_404(Pelicula.objects.order_by('anyo'))
+	context = {'lista_peliculas': peliculas }
+	return render(request, 'peliculas.html', context)
+
+def show_pelicula(request, pelicula_id):
+	personajesPelicula = []
+	pelicula = get_object_or_404(Pelicula, pk=pelicula_id)
+	personajes = get_list_or_404(Personaje)
+	for p in personajes:
+		for peli in p.pelicula.all():
+			if peli.titulo == pelicula.titulo:
+				personajesPelicula.append(p)
+	context = {'pelicula': pelicula, 'personajes': personajesPelicula}
+	return render(request, 'detailPelicula.html', context)
+
+def index(request):
+	personajes = get_list_or_404(Personaje.objects.order_by('raza'))
+	context = {'lista_personajes': personajes }
+	return render(request, 'index.html', context)
