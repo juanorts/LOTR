4b83e163c5115eb7c6758d4a1284e9632d7c3660
Juan Orts <91051695+juanorts@users.noreply.github.com>
2023-11-10 16:30:31 +0100

    Delete directory (error commit anterior)

--- lotr/lotr/appLOTR/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from django.contrib import admin
-from .models import Pelicula, Raza, Personaje
-admin.site.register(Pelicula)
-admin.site.register(Raza)
-admin.site.register(Personaje)
