d39f45bc5ed39e5c89a437cdc783a8c933725e1b
Ainhoa <murcia.ainhoa@opendeusto.es>
2023-12-10 17:17:51 +0100

    Cambios de js y de estandarizacion de los templates

---
+++ lotr/appLOTR/static/js/script.js
@@ -0,0 +1,79 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var imagen = document.getElementById("anillo-musical");
+    var audio = new Audio("/static/audio/lotr.mp3");
+
+    // Evento para reproducir la música
+    imagen.addEventListener("click",function() {
+        //Al hacer click en la imagen se reproduce la música
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    
+    });
+
+    var imagenesZoom = document.querySelectorAll(".imagenPersonaje");
+    var imagenesZoom2 = document.querySelectorAll("imagenPersonaje");
+
+    imagenesZoom2.forEach(function(imagen){
+        imagen.addEventListener("mouseover", function(){
+            aplicarZoom(imagen);
+        });
+        imagen.addEventListener('mouseout', function() {
+            // Restablece la transformación a su estado original
+            this.style.transform = "scale(1)";
+          });
+    });
+
+    imagenesZoom.forEach(function(imagen){
+        imagen.addEventListener("mouseover", function(){
+            aplicarZoom(imagen);
+        });
+        imagen.addEventListener('mouseout', function() {
+            // Restablece la transformación a su estado original
+            this.style.transform = "scale(1)";
+          });
+    });
+
+    function aplicarZoom(imagen){
+        var tamNuevo = "150%";       
+        imagen.style.transform = "scale("+tamNuevo+")";
+     
+    }
+
+    //Evento para mover a Gandalf
+    var gandalf = document.getElementById("gandalf");
+    var bocadillo = document.getElementById("imagen-oculta");
+
+    gandalf.addEventListener("click", function() {
+        var start = Date.now(); // timestamp inicial
+        var posOriginal = gandalf.style.right;
+
+        var temporizador = setInterval(function() {
+            
+            var tiempoPasado = Date.now() - start; // cuánto tiempo ha pasado desde el inicio
+            gandalf.style.right = tiempoPasado / 5.4 + 'px'; // movimiento lineal simple
+            
+            if (tiempoPasado > 2000){
+                clearInterval(temporizador);
+
+                bocadillo.style.display ="inline";   
+
+
+            } 
+
+            setTimeout(function(){
+
+                bocadillo.style.display ="none"; 
+
+                gandalf.style.right = posOriginal;
+
+            }, 4000);
+
+
+
+        }, 20);
+    });
+});
+
